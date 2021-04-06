ca05775ad26e848c56499f0dd28607bef3e8a9b6
Charles Wicklund <chwicklund@gmail.com>
2021-04-06 01:09:38 -0400

    Bug: fixed various errors

--- app/app.py
+++ app/app.py
@@ -34,6 +34,7 @@ def index():
 
     return render_template('index.html', title='Home', user=user, cities=cities_data)
 
+
 @app.route('/api/cities')
 def cities() -> str:
     js = json.dumps(cities_import())
